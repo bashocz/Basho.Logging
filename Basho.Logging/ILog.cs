cf433dedd691d88f2d37a96b1ddeab6c9a9491f8
Petr Baroš <pbaros@gmail.com>
2015-07-22 14:22:48 +0200

    added ILog interface

---
+++ Basho.Logging/ILog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basho.Logging
+{
+    public interface ILog
+    {
+        void Debug(string message);
+        Task DebugAsync(string message);
+
+        void Info(string message);
+        Task InfoAsync(string message);
+
+        void Warn(string message);
+        Task WarnAsync(string message);
+
+        void Error(string message);
+        Task ErrorAsync(string message);
+
+        void Fatal(string message);
+        Task FatalAsync(string message);
+    }
+}
