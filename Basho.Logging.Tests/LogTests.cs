744abc8d5f8e7aaf386d40c4ebd4e60d28bd177f
Petr Baroš <pbaros@gmail.com>
2015-07-22 15:22:26 +0200

    added unit test for constructor

---
+++ Basho.Logging.Tests/LogTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Basho.Logging.Tests.Mocks;
+using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basho.Logging.Tests
+{
+    [TestClass]
+    public class LogTests
+    {
+        private static MemoryAppender _appender = null;
+
+        [ClassInitialize]
+        public static void LogTestsInitialize(TestContext testContext)
+        {
+            _appender = new MemoryAppender();
+            BasicConfigurator.Configure(_appender);
+
+            var repository = LogManager.GetRepository();
+            repository.Threshold = Level.Debug;
+        }
+
+        [ClassCleanup]
+        public static void LogTestsCleanup()
+        {
+
+        }
+
+        [TestMethod]
+        public void CtorTest()
+        {
+            Foo foo = new Foo();
+
+            Assert.IsNotNull(LogManager.GetLogger(foo.GetType().Name));
+        }
+    }
+}
---
+++ Basho.Logging.Tests/Mocks/Foo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basho.Logging.Tests.Mocks
+{
+    class Foo
+    {
+        public static ILog Log = new Log();
+    }
+}
